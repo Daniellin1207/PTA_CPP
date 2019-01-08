56827a689be0eecc53c20d73a907354e0f13aa16
Daniel <linyili1224@163.com>
2019-01-08 10:00:38 +0800

    P22

---
+++ PTA_Train/A1024 Palindromic Number.cpp
@@ -0,0 +1,37 @@
+#include "pch.h"
+#define _CRT_SECURE_NO_DEPRECATE
+#include <cstdio>
+const int N = 54;
+char mp[5] = { 'S','H','C','D','J' };
+int start[N + 1], end[N + 1], next[N + 1];
+
+int main() {
+	int K;
+	scanf("%d", &K);
+	for (int i = 1; i <= N; i++)
+	{
+		start[i] = i;
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		scanf("%d", &next[i]);
+	}
+	for (int step = 0; step < K; step++)
+	{
+		for (int i = 1; i <= N; i++)
+		{
+			end[next[i]] = start[i];
+		}
+		for (int i = 1; i <= N; i++)
+		{
+			start[i] = end[i];
+		}
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		if (i != 1) printf(" ");
+		start[i]--;
+		printf("%c%d", mp[start[i] / 13], start[i] % 13 + 1);
+	}
+	return 0;
+}
