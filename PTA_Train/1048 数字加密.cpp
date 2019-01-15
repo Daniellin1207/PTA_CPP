1698fe86706652684b5baf499ec9670d1c40a9f0
Daniel <linyili1224@163.com>
2019-01-15 09:57:19 +0800

    1048&a1001&a1005 20190114 EVE

---
+++ PTA_Train/1048 数字加密.cpp
@@ -0,0 +1,46 @@
+#include "pch.h"
+#include <cstdio>
+#include <cstring>
+
+const int maxn = 110;
+char A[maxn], B[maxn], ans[maxn] = { 0 };
+
+void reverse(char s[]) {
+	int len = strlen(s);
+	for (int i = 0; i < len/2; i++)
+	{
+		int temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
+
+int main1048() {
+	scanf("%s %s", A, B);
+	reverse(A);
+	reverse(B);
+	int lenA = strlen(A);
+	int lenB = strlen(B);
+	int len = lenA > lenB ? lenA : lenB;
+	for (int i = 0; i < len; i++)
+	{
+		int numA = i < lenA ? A[i] - '0' : 0;
+		int numB = i < lenB ? B[i] - '0' : 0;
+		if (i%2==0)
+		{
+			int temp = (numB + numA) % 13;
+			if (temp == 10) ans[i] = 'J';
+			else if (temp == 11) ans[i] = 'Q';
+			else if (temp == 12) ans[i] = 'K';
+			else ans[i] = temp + '0';
+		}
+		else {
+			int temp = numB - numA;
+			if (temp < 0) temp += 10;
+			ans[i] = temp + '0';
+		}
+	}
+	reverse(ans);
+	puts(ans);
+	return 0;
+}
---
+++ PTA_Train/A1001 A+B Format.cpp
@@ -0,0 +1,32 @@
+#include "pch.h"
+#include <cstdio>
+
+int main1001a() {
+	int a, b, c,temp;
+	scanf("%d %d", &a, &b);
+	c = a + b;
+
+	if (c < 0) {
+		c = -c;
+		printf("-");
+	}
+
+	if (c < 1000) printf("%d", c);
+	else if (c < 1000000) printf("%d,%03d", c / 1000, c % 1000);
+	else printf("%d,%03d,%03d", c / 1000000, c % 1000000 / 1000, c % 1000);
+	return 0;
+}
+
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main() {
+//	int a, b, sum, c;
+//	scanf("%d %d", &a, &b);
+//	sum = a + b;
+//	c = abs(sum);
+//	if (c < 1000) printf("%d", sum);
+//	else if (c < 1000000) printf("%d,%03d", sum / 1000, c % 1000);
+//	else printf("%d,%03d,%03d", sum / 1000000, (c / 1000) % 1000, c % 1000);
+//	return 0;
+//}
