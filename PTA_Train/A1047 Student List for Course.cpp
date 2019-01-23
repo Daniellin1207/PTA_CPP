b90884ee585164afa2b2ff38f909171cee9651fd
Daniel <linyili1224@163.com>
2019-01-23 15:36:53 +0800

    1019&1039a&1047a&1060a&1063a 20190123 P.M

---
+++ PTA_Train/A1047 Student List for Course.cpp
@@ -0,0 +1,85 @@
+#include "pch.h"
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int maxn = 40010;
+const int maxc = 2510;
+char name[maxn][5]; // 将name存储在另一个数组中，由int数组调取
+vector<int> course[maxc];
+bool cmp(int a, int b) {
+	return strcmp(name[a], name[b]) < 0;
+}
+int main1047a() {
+	int n, k, c, courseID;
+	scanf("%d%d", &n, &k);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%s %d", name[i], &c);
+		for (int j = 0; j < c; j++)
+		{
+			scanf("%d", &courseID);
+			course[courseID].push_back(i);
+		}
+	}
+	for (int i = 1; i <= k; i++)
+	{
+		printf("%d %d\n", i, course[i].size());
+		sort(course[i].begin(), course[i].end(), cmp);
+		for (int j = 0; j < course[i].size(); j++)
+		{
+			printf("%s\n", name[course[i][j]]);
+		}
+	}
+	return 0;
+}
+
+
+
+//vector<string> courseVec[2510];
+//
+////int getID(char name[]) {
+////	int id=0;
+////	for (int i = 0; i < 3; i++)
+////	{
+////		id = id * 26 + name[i] - 'A';
+////	}
+////	id = id * 26 + name[3] - '0';
+////	return id;
+////}
+//
+//int main() {
+//	int n, m;
+//	scanf("%d%d", &n, &m);
+//	for (int i = 0; i < n; i++)
+//	{
+//		char name[5];
+//		int k;
+//		scanf("%s %d", name, &k);
+//		//printf("hello: %s\n", name);
+//		//int id = getID(name);
+//		for (int j = 0; j < k; j++)
+//		{
+//			int course;
+//			scanf("%d", &course);
+//			courseVec[course].push_back(name);
+//		}
+//	}
+//	int flag = false;
+//	for (int i = 0; i < 2510; i++)
+//	{
+//		if (courseVec[i].size() > 0) {
+//			if (flag) printf("\n");
+//			flag = true;
+//			sort(courseVec[i].begin(), courseVec[i].end());
+//			printf("%d %d", i, courseVec[i].size());
+//			for (int j = 0; j < courseVec[i].size(); j++)
+//			{
+//				printf("\n%s", courseVec[i][j]);
+//			}
+//		}
+//	}
+//	return 0;
+//}
---
+++ PTA_Train/A1060 Are They Equal.cpp
@@ -0,0 +1,57 @@
+#include "pch.h"
+#include <string>
+#include <iostream>
+using namespace std;
+int n;
+string deal(string s, int& e) {
+	int k = 0;
+	while (s.length() > 0 && s[0] == '0') {
+		s.erase(s.begin());
+	}
+	if (s[0] == '.') {
+		s.erase(s.begin());
+		while (s.length() > 0 && s[0] == '0') {
+			s.erase(s.begin());
+			e--;
+		}
+	}
+	else {
+		while (k < s.length() && s[k] != '.') {
+			k++;
+			e++;
+		}
+		if (k < s.length()) {
+			s.erase(s.begin() + k);
+		}
+	}
+	if (s.length() == 0) {
+		e = 0;
+	}
+	int num = 0;
+	k = 0;
+	string res;
+	while (num < n) {
+		if (k < s.length()) res += s[k++];
+		else res += '0';
+		num++;
+	}
+	return res;
+}
+int main1060a() {
+	string s1, s2, s3, s4;
+	cin >> n >> s1 >> s2;
+	int e1 = 0, e2 = 0;
+	s3 = deal(s1, e1);
+	s4 = deal(s2, e2);
+	if (s3 == s4 && e1 == e2) {
+		cout << "YES 0." << s3 << "*10^" << e1 << endl;
+		//printf("YES 0.%s*10^%d\n", s3.c_str(), e1);
+	}
+	else {
+
+		cout << "NO 0." << s3 << "*10^" << e1 << " 0." << s4 << "*10^" << e2 << endl;
+		//printf("NO 0.%s*10^%d", s3.c_str(), e1);
+		//printf(" 0.%s*10^%d\n", s4.c_str(), e2);
+	}
+	return 0;
+}
