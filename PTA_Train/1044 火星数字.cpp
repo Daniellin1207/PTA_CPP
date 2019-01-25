aae37b648fe9b1fedb06c060532b969f677384f7
Daniel <linyili1224@163.com>
2019-01-25 11:43:13 +0800

    1044&1060a&1091a&1097a&1103a 20190125 A.M. 深度搜索(迭代)与广度搜索(穷举) 值得再看

--- PTA_Train/1044 火星数字.cpp
+++ PTA_Train/1044 火星数字.cpp
@@ -5,47 +5,47 @@
 #include <iostream>
 using namespace std;
 
-string unitDigit[13] = { "tret","jan","feb","mar","apr","may","jun","jly","aug","sep","oct","nov","dec" };
-string tenDigit[13] = { "tret","tam","hel","maa","huh","tou","kes","hei","elo","syy","lok","mer","jou" };
-string numToStr[170];
-map<string, int> strToNum; // 字典保存 字符(任意)=>>整型(任意) 映射
-						   // 数组保存 整型=>>其他 映射
-void init() {
-	for (int i = 0; i < 13; i++)
-	{
-		numToStr[i] = unitDigit[i];
-		strToNum[unitDigit[i]] = i;
-		numToStr[i * 13] = tenDigit[i];
-		strToNum[tenDigit[i]] = 13 * i;
-	}
-	for (int i = 1;	i < 13; i++)
-	{
-		for (int j = 1; j < 13; j++)
-		{
-			string str = tenDigit[i] + " " + unitDigit[j];
-			numToStr[i * 13 + j] = str;
-			strToNum[str] = i * 13 + j;
-		}
-	}
-}
-int main1044() {
-	init();
-	int T;
-	scanf("%d%*c", &T);
-	while (T--) {
-		string str;
-		getline(cin, str);
-		if (str[0] >= '0' && str[0] <= '9') {
-			int num = 0;
-			for (int i = 0; i < str.length(); i++)
-			{
-				num = num * 10 + (str[i] - '0');
-			}
-			cout << numToStr[num] << endl;
-		}
-		else {
-			cout << strToNum[str] << endl;
-		}
-	}
-	return 0;
-}
+//string unitDigit[13] = { "tret","jan","feb","mar","apr","may","jun","jly","aug","sep","oct","nov","dec" };
+//string tenDigit[13] = { "tret","tam","hel","maa","huh","tou","kes","hei","elo","syy","lok","mer","jou" };
+//string numToStr[170];
+//map<string, int> strToNum; // 字典保存 字符(任意)=>>整型(任意) 映射
+//						   // 数组保存 整型=>>其他 映射
+//void init() {
+//	for (int i = 0; i < 13; i++)
+//	{
+//		numToStr[i] = unitDigit[i];
+//		strToNum[unitDigit[i]] = i;
+//		numToStr[i * 13] = tenDigit[i];
+//		strToNum[tenDigit[i]] = 13 * i;
+//	}
+//	for (int i = 1;	i < 13; i++)
+//	{
+//		for (int j = 1; j < 13; j++)
+//		{
+//			string str = tenDigit[i] + " " + unitDigit[j];
+//			numToStr[i * 13 + j] = str;
+//			strToNum[str] = i * 13 + j;
+//		}
+//	}
+//}
+//int main1044() {
+//	init();
+//	int T;
+//	scanf("%d%*c", &T);
+//	while (T--) {
+//		string str;
+//		getline(cin, str);
+//		if (str[0] >= '0' && str[0] <= '9') {
+//			int num = 0;
+//			for (int i = 0; i < str.length(); i++)
+//			{
+//				num = num * 10 + (str[i] - '0');
+//			}
+//			cout << numToStr[num] << endl;
+//		}
+//		else {
+//			cout << strToNum[str] << endl;
+//		}
+//	}
+//	return 0;
+//}
---
+++ PTA_Train/A1103 Integer Factorization.cpp
@@ -0,0 +1,55 @@
+#include "pch.h"
+#include <cstdio>
+#include <vector>
+#include <algorithm>	
+using namespace std;
+
+//int n, k, p, maxFacSum = -1;
+//vector<int> fac, ans, temp;
+//int power(int x) {
+//	int ans = 1;
+//	for (int i = 0; i < p; i++)
+//	{
+//		ans *= x;
+//	}
+//	return ans;
+//}
+//void init() {
+//	int i = 0, temp = 0;
+//	while (temp <= n) {
+//		fac.push_back(temp);
+//		temp = power(++i);
+//	}
+//}
+//
+//void DFS(int index, int nowK, int sum, int facSum) {
+//	if (sum == n && nowK == k) {
+//		if (facSum > maxFacSum) {
+//			ans = temp;
+//			maxFacSum = facSum;
+//		}
+//		return;
+//	}
+//	if (sum > n || nowK > k)return;
+//	if (index - 1 >= 0) {
+//		temp.push_back(index);
+//		DFS(index, nowK + 1, sum + fac[index], facSum + index);
+//		temp.pop_back();
+//		DFS(index - 1, nowK, sum, facSum);
+//	}
+//}
+//
+//int main1103a() {
+//	scanf("%d%d%d", &n, &k, &p);
+//	init();
+//	DFS(fac.size() - 1, 0, 0, 0);
+//	if (maxFacSum == -1) printf("Impossible\n");
+//	else {
+//		printf("%d = %d^%d", n, ans[0], p);
+//		for (int i = 1; i < ans.size(); i++)
+//		{
+//			printf(" + %d^%d", ans[i], p);
+//		}
+//	}
+//	return 0;
+//}
